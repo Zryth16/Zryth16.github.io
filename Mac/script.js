f0f362e2bfc2c5d1869a202c86d2ad63b6fd3349
Zryth16 <75670030+Zryth16@users.noreply.github.com>
2025-04-08 17:02:22 +0100

    Add files via upload

--- Mac/script.js
+++ Mac/script.js
@@ -1,9 +1,18 @@
 $(document).ready(function () {
-    // $('.slider').slick({
-    //     dots: true,
-    //     infinite: true,
-    //     speed: 500,
-    //     slidesToShow: 1,
-    //     adaptiveHeight: true
-    // });
+    $('.slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 250,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        autoplay: true, // Enable autoplay
+        autoplaySpeed: 3000 // Set autoplay speed in milliseconds (3 seconds)
+    });
+
+    $('.scroll-down-link').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $('section.services').offset().top
+        }, 800); // Adjust the duration (800ms) as needed
+    });
 });
